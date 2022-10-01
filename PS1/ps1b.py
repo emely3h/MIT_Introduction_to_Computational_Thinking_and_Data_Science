642626fa8dc33d1c294eaf13b30ff9efefa1bfd9
Emely Henninger <hee3be@bosch.com>
2022-10-01 18:07:41 +0200

    add 1b solution

--- PS1/ps1b.py
+++ PS1/ps1b.py
@@ -9,28 +9,31 @@
 # Part B: Golden Eggs
 #================================
 
+
 # Problem 1
-def dp_make_weight(egg_weights, target_weight, memo = {}):
-    """
-    Find number of eggs to bring back, using the smallest number of eggs. Assumes there is
-    an infinite supply of eggs of each weight, and there is always a egg of value 1.
-    
-    Parameters:
-    egg_weights - tuple of integers, available egg weights sorted from smallest to largest value (1 = d1 < d2 < ... < dk)
-    target_weight - int, amount of weight we want to find eggs to fit
-    memo - dictionary, OPTIONAL parameter for memoization (you may not need to use this parameter depending on your implementation)
-    
-    Returns: int, smallest number of eggs needed to make target weight
-    """
-    # TODO: Your code here
-    pass
+memo = {}
+def dp_make_weight(egg_weights, target_weight):
+    if target_weight in egg_weights:
+        return 1
+    egg_list = []
+    for egg in egg_weights:
+        new_weight = target_weight - egg
+        if new_weight in memo:
+            egg_list.append(memo[new_weight])
+        elif new_weight >= 0:
+            egg_list.append(dp_make_weight(egg_weights, new_weight))
+    min_count = min(egg_list) + 1
+    memo[target_weight] = min_count
+    return min_count
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
     egg_weights = (1, 5, 10, 25)
+    egg_weights2 = (1, 3, 2)
+    n2 = 10
     n = 99
-    print("Egg weights = (1, 5, 10, 25)")
-    print("n = 99")
-    print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
-    print("Actual output:", dp_make_weight(egg_weights, n))
-    print()
+    # print("Egg weights = (1, 5, 10, 25)")
+    # print("n = 99")
+    # print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
+    print(dp_make_weight(egg_weights2, n2))
+    #print(test(4))
